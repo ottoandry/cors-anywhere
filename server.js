bb48dddabfc20dfedb06e28ea7c8e7ba46fce0bc
Andrea Ottomanelli <ottoandry@hotmail.it>
2021-07-25 22:04:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
+  requireHeader: ['origin', 'x-requested-with', 'cors-otto'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
